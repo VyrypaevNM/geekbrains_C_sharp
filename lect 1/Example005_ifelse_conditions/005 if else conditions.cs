b4794192d3e3d28a4239a0d6687e99c8f83efe50
Nick <st_vnm@mail.ru>
2023-06-10 23:51:57 +0300

    if else conditions added

---
+++ lect 1/Example005_ifelse_conditions/005 if else conditions.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input user name");
+string username = Console.ReadLine();
+if (username.ToLower() == "nick")
+{
+    Console.WriteLine("Wow, it's Nick!");
+}
+else {Console.Write("Hi, ");Console.WriteLine(username);}
