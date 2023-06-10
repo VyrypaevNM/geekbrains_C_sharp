26f90e77e50a39e06b7f09e606c83b134b7e13aa
Nick <st_vnm@mail.ru>
2023-06-10 23:03:32 +0300

    random and division added

---
+++ lect 1/Example004_random_division/004 random division.cs
@@ -0,0 +1,11 @@
+﻿// ввод случайного числа
+int num1 = new Random().Next(1, 10);
+Console.Write("Input number, please = ");
+double num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("random number = ");
+Console.WriteLine(num1);
+Console.Write(num1);
+Console.Write("/");
+Console.Write(num2);
+Console.Write("=");
+Console.WriteLine(num1 / num2);
