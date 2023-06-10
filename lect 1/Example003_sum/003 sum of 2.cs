ba6a5186bdd8585251080f10f52c34fdbda4c28e
Nick <st_vnm@mail.ru>
2023-06-10 22:40:28 +0300

    sum of 2 numbers added

---
+++ lect 1/Example003_sum/003 sum of 2.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input first number, please");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number, please");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(num1 + num2);
