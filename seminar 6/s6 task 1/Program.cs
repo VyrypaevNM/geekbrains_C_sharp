a0abd0cc45344961819c36fa99259e7fba7a44d7
Nick <st_vnm@mail.ru>
2023-07-04 22:41:41 +0300

    seminar 6 homework added

---
+++ seminar 6/s6 task 1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+
+int counter = 0;
+int number = -1;
+while (number < 1) 
+{
+    number = Input("Input positive quantity of numbers: ");
+}
+
+for (int i = 0; i < number; i++)
+    {
+        int temp = Input($"Input number № {i+1}: ");
+        if (temp > 0) {counter++;}
+    }
+
+System.Console.WriteLine($"Quantity of positive numbers equal {counter}");
+
+int Input(string message)
+{
+    int num;
+    Console.Write(message);
+    num = Convert.ToInt32(Console.ReadLine());
+    return(num);
+}
---
+++ seminar 6/s6 task 2/Program.cs
@@ -0,0 +1,35 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+
+Console.Clear();
+
+
+double k1 = Input("Input coefficient k for first line k1= ");
+double b1 = Input("Input coefficient b for first line b1= ");
+double k2 = Input("Input coefficient k for first line k2= ");
+double b2 = Input("Input coefficient b for first line b2= ");
+
+if ((k1 == k2) & (b1 == b2))
+{
+    System.Console.WriteLine("The lines are same");
+}
+else if (k1 == k2)
+{
+    System.Console.WriteLine("The lines have no any intersections");
+}
+else
+{
+    double X = (b2-b1)/(k1-k2);
+    double Y = k1 * X + b1;
+    System.Console.WriteLine($"Lines Y = {k1}*X +{b1} and Y = {k2}*X +{b2} have intersection in dot X = {X}, Y = {Y}");
+}
+
+double Input(string message)
+{
+    double num;
+    Console.Write(message);
+    num = Convert.ToDouble(Console.ReadLine());
+    return(num);
+}
