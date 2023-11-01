afea8d1bdd40e8a413820ecb4494e7a00a314df6
Nick <st_vnm@mail.ru>
2023-11-01 21:03:43 +0300

    code added

---
+++ final task/Program.cs
@@ -0,0 +1,32 @@
+﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+
+Console.Clear();
+SecondArrayWithIF(array1, array2);
+System.Console.WriteLine("Old array:");
+PrintArray(array1);
+System.Console.WriteLine();
+System.Console.WriteLine("New array:");
+PrintArray(array2);
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
